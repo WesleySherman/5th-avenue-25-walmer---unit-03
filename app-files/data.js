4973bc193d7d220dfc0188146c72516b19f4f91f
Wesley Sherman <wesleysherman92@gmail.com>
2025-06-10 16:09:05 +0200

    initial commit

---
+++ app-files/data.js
@@ -0,0 +1,424 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01---front-of-property",
+      "name": "01 - Front of Property",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.13941346708326563,
+          "pitch": 0.1341677621025248,
+          "rotation": 0,
+          "target": "1-02---lobby"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02---lobby",
+      "name": "02 - Lobby",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9813427590714241,
+          "pitch": 0.18142098404319285,
+          "rotation": 0,
+          "target": "0-01---front-of-property"
+        },
+        {
+          "yaw": -0.6525614265784032,
+          "pitch": 0.12400529570973617,
+          "rotation": 0,
+          "target": "2-03---bar-area-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03---bar-area-01",
+      "name": "03 - Bar Area 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.407332602069305,
+          "pitch": 0.15339537958609384,
+          "rotation": 0,
+          "target": "1-02---lobby"
+        },
+        {
+          "yaw": -2.329958835364092,
+          "pitch": 0.20237148277471562,
+          "rotation": 0,
+          "target": "3-04---bar-area-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04---bar-area-02",
+      "name": "04 - Bar Area 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.077852019657753,
+          "pitch": 0.21006106334551156,
+          "rotation": 0,
+          "target": "2-03---bar-area-01"
+        },
+        {
+          "yaw": 2.0055400088267543,
+          "pitch": 0.21274327239146018,
+          "rotation": 0,
+          "target": "7-08---bathrooms-"
+        },
+        {
+          "yaw": 0.873766244611117,
+          "pitch": 0.08895287466314983,
+          "rotation": 0,
+          "target": "4-05---kitchen-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05---kitchen-area",
+      "name": "05 - Kitchen Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.018696965615753,
+          "pitch": 0.2575120819550101,
+          "rotation": 0,
+          "target": "3-04---bar-area-02"
+        },
+        {
+          "yaw": 1.0128734757717943,
+          "pitch": 0.34922312152882995,
+          "rotation": 0,
+          "target": "5-06---kitchen-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06---kitchen-area",
+      "name": "06 - Kitchen Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7077589224276828,
+          "pitch": 0.29812822945946316,
+          "rotation": 0,
+          "target": "4-05---kitchen-area"
+        },
+        {
+          "yaw": -0.11554477011910613,
+          "pitch": -0.028423809311858506,
+          "rotation": 0,
+          "target": "3-04---bar-area-02"
+        },
+        {
+          "yaw": -2.7231676202007886,
+          "pitch": 0.2719352595108617,
+          "rotation": 7.0685834705770345,
+          "target": "8-09---back-of-property"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-07---store-room",
+      "name": "07 - Store Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-08---bathrooms-",
+      "name": "08 - Bathrooms ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6167021828576837,
+          "pitch": 0.47660883660521236,
+          "rotation": 1.5707963267948966,
+          "target": "3-04---bar-area-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-09---back-of-property",
+      "name": "09 - Back of Property",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10510900355779818,
+          "pitch": 0.19027598051278005,
+          "rotation": 0,
+          "target": "5-06---kitchen-area"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "5th Avenue 25, Walmer - Unit 03",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
